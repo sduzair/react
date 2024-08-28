9507bf186635e98601d3409457ec287809159812
sduzair <sduzairahmed@gmail.com>
2024-08-28 06:02:11 -0400

    feat: fetching products and Bootstrap styling

--- hook-reducer-cart/src/App.tsx
+++ hook-reducer-cart/src/App.tsx
@@ -1,10 +1,98 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import styles from "./App.module.css";
 
-export function App() {
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then<ProductsWithPagination>((res) => res.json())
+      .then((productsWithPagination) => {
+        setProducts(productsWithPagination.products);
+      });
+  }, []);
+
   return (
     <>
-      <h1 className={`font-monospace ${styles["app-italic"]}`}>Hello World!</h1>
+      <div className="container mt-4">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {products.map((product) => (
+            <div key={product.id} className="col">
+              <div className="card h-100">
+                <img
+                  src={product.thumbnail}
+                  className="card-img-top"
+                  alt={product.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">{product.description}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Price: ${product.price}</li>
+                  <li className="list-group-item">Rating: {product.rating}</li>
+                  <li className="list-group-item">Brand: {product.brand}</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
+
+interface ProductsWithPagination {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand?: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: Meta;
+  images: string[];
+  thumbnail: string;
+}
+
+interface Meta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+interface Review {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export default App;
