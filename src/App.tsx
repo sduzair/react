ba1474726024b21a7bfb85bb0e479c04408b6c0d
sduzair <sduzairahmed@gmail.com>
2024-09-10 22:31:02 -0400

    chore: added src folder (were missing) - tried branching a216ef4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <h1 className={`font-monospace ${styles["app-italic"]}`}>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
