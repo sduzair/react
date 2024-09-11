a216ef4d0750288e974a802ef2c2c0af18e536dd
sduzair <sduzairahmed@gmail.com>
2024-09-10 20:18:16 -0400

    chore: add Prettier configuration and workplace extension recommendation

---
+++ hook-reducer-cart/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import styles from "./App.module.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className={`font-monospace ${styles["app-italic"]}`}>Hello World!</h1>
+    </>
+  );
+}
