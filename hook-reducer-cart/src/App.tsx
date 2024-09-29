328477947e0854ce65aea1daf6a52ca2dcaabd78
sduzair <sduzairahmed@gmail.com>
2024-09-29 04:59:07 -0400

    feat: enhance cart functionality and styling
    - Implemented useReducer for better state management in the cart provider.
    - Improved the OffcanvasCart component to display items with quantity and subtotal.
    - Added custom styles for offcanvas elements to improve UI.
    - Introduced a utility function to format prices consistently across components.

--- hook-reducer-cart/src/App.tsx
+++ hook-reducer-cart/src/App.tsx
@@ -1,10 +1,17 @@
 import "bootstrap/js/src/collapse.js";
 import "bootstrap/js/src/offcanvas.js";
-import { ComponentProps, ReactNode, useEffect, useState } from "react";
+import {
+  ComponentProps,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import "./App.css";
 import styles from "./App.module.css";
 
-import { OffcanvasCart } from "./Offcanvas";
+import { formatPrice, OffcanvasCart } from "./Offcanvas";
+import { CartContext } from "./CartProvider";
 
 function App() {
   return (
@@ -16,14 +23,28 @@ function App() {
         <Navbar.Button
           data-bs-target="#offcanvasCart"
           aria-controls="offcanvasCart"
+          data-bs-toggle="offcanvas"
         ></Navbar.Button>
       </Navbar>
       <OffcanvasCart id="offcanvasCart" aria-labelledby="offcanvasCartLabel">
-        <OffcanvasCart.Header>
-          <OffcanvasCart.Header.Title id="offcanvasCartLabel">
-            My Cart
-          </OffcanvasCart.Header.Title>
-        </OffcanvasCart.Header>
+        <div className="offcanvas-header">
+          <div className="col">
+            <OffcanvasCart.Title
+              id="offcanvasCartLabel"
+              className="offcanvas-title"
+            >
+              My Cart
+            </OffcanvasCart.Title>
+          </div>
+          <div className="col-auto">
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
         <OffcanvasCart.Body></OffcanvasCart.Body>
       </OffcanvasCart>
       <ProductsComponent />
@@ -42,11 +63,11 @@ function Navbar({ children }: { children: ReactNode }) {
 Navbar.Button = Button;
 
 function Button(props: ComponentProps<"button">) {
+  const [cart, _] = useContext(CartContext);
   return (
     <button
-      className="btn btn-primary"
+      className="btn btn-primary position-relative m-1 mx-2"
       type="button"
-      data-bs-toggle="offcanvas"
       {...props}
     >
       <svg
@@ -59,8 +80,18 @@ function Button(props: ComponentProps<"button">) {
       >
         <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
       </svg>
+
+      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+        {formatCountForCartBadge(cart.count)}
+        <span className="visually-hidden">unread messages</span>
+      </span>
     </button>
   );
+}
+
+function formatCountForCartBadge(count: number) {
+  if (count > 10) return "10+";
+  return `${count}`;
 }
 
 function ProductsComponent() {
@@ -154,7 +185,9 @@ function ProductCard({ product }: { product: Product }) {
           <p className="card-text">{product.description}</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">Price: ${product.price}</li>
+          <li className="list-group-item">
+            Price: {formatPrice(product.price)}
+          </li>
           <li className="list-group-item">Rating: {product.rating}</li>
           <li className="list-group-item">Brand: {product.brand}</li>
         </ul>
--- hook-reducer-cart/src/Offcanvas.tsx
+++ hook-reducer-cart/src/Offcanvas.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps, ReactNode, useContext } from "react";
-import { Cart, CartContext } from "./CartProvider";
+import { CartContext } from "./CartProvider";
+import styles from "./Offcanvas.module.css";
 
 export function OffcanvasCart({
   children,
@@ -7,9 +8,8 @@ export function OffcanvasCart({
 }: { children: ReactNode } & ComponentProps<"div">) {
   return (
     <div
-      className="offcanvas offcanvas-end offcanvasCustom"
+      className={`offcanvas offcanvas-end ${styles["offcanvas-custom"]}`}
       data-bs-scroll="true"
-      data-bs-backdrop="false"
       tabIndex={-1}
       aria-modal="true"
       role="dialog"
@@ -20,56 +20,101 @@ export function OffcanvasCart({
   );
 }
 
-OffcanvasCart.Header = Header;
-Header.Title = Title;
+OffcanvasCart.Title = Title;
 OffcanvasCart.Body = Body;
-
-function Header({ children }: { children: ReactNode } & ComponentProps<"div">) {
-  return (
-    <div className="offcanvas-header">
-      {children}
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="offcanvas"
-        aria-label="Close"
-      ></button>
-    </div>
-  );
-}
 
 function Title({
   children,
   ...props
 }: { children: ReactNode } & ComponentProps<"h5">) {
+  const [cart, _] = useContext(CartContext);
   return (
-    <h5 className="offcanvas-title" {...props}>
-      {children}
-    </h5>
+    <div className="d-flex align-items-center justify-content-between">
+      <h5 {...props}>{children}</h5>
+      <span className="badge rounded-pill text-bg-success fs-6">
+        {formatPrice(cart.total)}
+      </span>
+    </div>
   );
 }
 
 function Body() {
-  const [cart, setCart] = useContext(CartContext);
+  const [cart, dispatch] = useContext(CartContext);
   return (
-    <div className="offcanvas-body">
-      {cart!.map((item) => (
-        <div key={item.id} className="col">
-          <div>{item.title}</div>
-          <div>{item.description}</div>
-          <div>{item.category}</div>
-          <div>{item.price}</div>
+    <>
+      <div className={`${styles["offcanvas-col-names"]} pt-0 pb-0`}>
+        <div className="row gx-1">
+          <div className="col-7 fw-medium">Item</div>
+          <div className="col-2 fw-medium">Qty</div>
+          <div className="col-3 fw-medium">Subtotal</div>
         </div>
-      ))}
-      <button
-        onClick={() =>
-          setCart((prevCart: Cart | undefined) => [
-            { ...prevCart![0], category: prevCart![0].category + "1" },
-          ])
-        }
-      >
-        Update Cart
-      </button>
-    </div>
+      </div>
+      <div className="offcanvas-body">
+        {Object.values(cart.items).map((item) => (
+          <div key={item.id} className={`card ${styles["card"]} mb-3`}>
+            <div className="row g-0">
+              <div className="col">
+                <img
+                  src={`${item.images[0]}`}
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div className="col">
+                <div className="card-body d-flex flex-column h-100">
+                  <p className={`card-title ${styles["card-title"]}`}>
+                    {item.title}
+                  </p>
+                  <p className="card-subtitle">{item.category}</p>
+                  <p className="card-text mt-auto">{formatPrice(item.price)}</p>
+                </div>
+              </div>
+              <div className={`col-2 ${styles["item-quantity"]}`}>
+                {item.quantity}
+              </div>
+              <div className={`col-3 ${styles["item-subtotal"]}`}>
+                {formatPrice(item.subtotal)}
+              </div>
+            </div>
+            <div className="row justify-content-end gx-2 m-2">
+              <div className="col-auto">
+                <button
+                  className="btn btn-primary"
+                  onClick={() =>
+                    dispatch({
+                      type: "ADDITEM",
+                      payload: { cartItem: { ...item, quantity: 1 } },
+                    })
+                  }
+                >
+                  Add
+                </button>
+              </div>
+
+              <div className="col-auto">
+                <button
+                  className="col btn btn-danger"
+                  onClick={() =>
+                    dispatch({
+                      type: "REMOVEITEM",
+                      payload: { itemId: item.id },
+                    })
+                  }
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
 }
